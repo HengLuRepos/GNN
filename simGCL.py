b9a8934a69122014622ab0244e4e9f4ce0340629
HengLuRepos <1505613730@qq.com>
2023-11-14 11:17:11 -0500

    batch cl loss

--- simGCL.py
+++ simGCL.py
@@ -29,7 +29,7 @@ class SimGCL(LightGCN):
         out = torch.mean(embs, dim=1)
         users, items = torch.split(out, [self.num_user, self.num_item])
         return users, items
-    def cl_loss(self, users, items):
+    def cl_loss(self):
         user_noise_1, items_noise_1 = self.noise_propagate()
         user_noise_2, items_noise_2 = self.noise_propagate()
 
@@ -53,3 +53,33 @@ class SimGCL(LightGCN):
         cl_item_loss = torch.sum(-torch.log(pos_item_pairs/(all_item_pairs - pos_item_pairs)))
 
         return cl_user_loss + cl_item_loss
+    
+    def batch_cl_loss(self, users, items):
+        user_noise_1, items_noise_1 = self.noise_propagate()
+        user_noise_2, items_noise_2 = self.noise_propagate()
+
+        user_noise_1 = user_noise_1[users]
+        user_noise_2 = user_noise_2[users]
+        user_noise_1 /= torch.norm(user_noise_1, p=2, dim=1, keepdim=True)
+        user_noise_2 /= torch.norm(user_noise_2, p=2, dim=1, keepdim=True)
+
+        pos_user_pairs = torch.sum(user_noise_1 * user_noise_2, axis=1)
+        pos_user_pairs = torch.exp(pos_user_pairs / self.tau) 
+        all_user_pairs = torch.matmul(user_noise_1, user_noise_2.T)
+        all_user_pairs = torch.exp(all_user_pairs / self.tau).sum(axis=1)
+        cl_user_loss = torch.sum(-torch.log(pos_user_pairs/(all_user_pairs - pos_user_pairs)))
+
+        item_index = np.array(items).flatten()
+        items_noise_1 = items_noise_1[item_index]
+        items_noise_2 = items_noise_2[item_index]
+        items_noise_1 /= torch.norm(items_noise_1, p=2, dim=1, keepdim=True)
+        items_noise_2 /= torch.norm(items_noise_2, p=2, dim=1, keepdim=True)
+
+        pos_item_pairs = torch.sum(items_noise_1 * items_noise_2, axis=1)
+        pos_item_pairs = torch.exp(pos_item_pairs / self.tau)
+        all_item_pairs = torch.matmul(items_noise_1, items_noise_2.T)
+        all_item_pairs = torch.exp(all_item_pairs / self.tau).sum(axis=1)
+        cl_item_loss = torch.sum(-torch.log(pos_item_pairs/(all_item_pairs - pos_item_pairs)))
+
+        return cl_user_loss + cl_item_loss
+
