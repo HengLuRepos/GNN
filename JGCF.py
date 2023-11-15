ed1e9da54d043eddb89e115a62413d81290204e4
HengLuRepos <1505613730@qq.com>
2023-11-14 20:40:38 -0500

    fix bugs

--- JGCF.py
+++ JGCF.py
@@ -19,21 +19,15 @@ class JGCF(LightGCN):
         item_weight = self.embedding_item.weight
         embed = torch.cat([user_weight, item_weight])
 
-        p0 = eye.to_sparse_csr()
-        p1 = (self.a - self.b)/2 * eye + (self.a + self.b)/2 * self.graph
-
-        pk = [p0, p1.to_sparse_csr()]
+        embs = [torch.sparse.mm(eye, embed), torch.sparse.mm(p1, embed)]
         for k in range(2, self.layers + 1):
             theta_1 = (2*k + self.a + self.b) * (2*k + self.a + self.b - 1) / ((k + self.a + self.b) * 2*k)
             theta_2 = ((2*k + self.a + self.b - 1)*(self.a**2 - self.b**2)) /((2*k + self.a + self.b - 2) * (k + self.a + self.b) * 2 * k)
-            temp = theta_1*self.graph + theta_2*eye
-            p = torch.sparse.mm(temp.to_sparse_csr(), pk[-1]).to_sparse_csr()
-            del temp
+            emb_k = theta_1 * torch.sparse.mm(self.graph, embs[-1]) + theta_2 * embs[-1]
             theta_3 = ((k + self.a - 1) * (k + self.b - 1) * (2*k + self.a + self.b)) / (k*(self.a + self.b + k)*(2*k + self.a + self.b -2))
-            temp = -theta_3 * pk[-2]
-            p = p + temp.to_sparse_csr()
-            pk.append(p.to_sparse_csr())
-        band_stop = torch.stack([torch.sparse.mm(p, embed) for p in pk], dim=1).mean(dim=1)
+            emb_k = -theta_3 * embs[-2]
+            embs.append(emb_k)
+        band_stop = torch.stack(embs, dim=1).mean(dim=1)
         band_pass = torch.tanh(self.alpha * embed - band_stop)
         out = torch.hstack([band_stop, band_pass])
         users, items = torch.split(out, [self.num_user, self.num_item])
