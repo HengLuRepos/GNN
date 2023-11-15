cfa0cafecd3d835ffad21267cce2325c3a5b7cd5
HengLuRepos <1505613730@qq.com>
2023-11-14 21:49:52 -0500

    fix bugs

--- simGCL.py
+++ simGCL.py
@@ -60,26 +60,25 @@ class SimGCL(LightGCN):
 
         user_noise_1 = user_noise_1[users]
         user_noise_2 = user_noise_2[users]
-        user_noise_1 /= torch.norm(user_noise_1, p=2, dim=1, keepdim=True)
-        user_noise_2 /= torch.norm(user_noise_2, p=2, dim=1, keepdim=True)
+        user_noise_1_norm = user_noise_1 / torch.norm(user_noise_1, p=2, dim=1, keepdim=True)
+        user_noise_2_norm = user_noise_2 / torch.norm(user_noise_2, p=2, dim=1, keepdim=True)
 
-        pos_user_pairs = torch.sum(user_noise_1 * user_noise_2, axis=1)
+        pos_user_pairs = torch.sum(user_noise_1_norm * user_noise_2_norm, axis=1)
         pos_user_pairs = torch.exp(pos_user_pairs / self.tau) 
-        all_user_pairs = torch.matmul(user_noise_1, user_noise_2.T)
+        all_user_pairs = torch.matmul(user_noise_1_norm, user_noise_2_norm.T)
         all_user_pairs = torch.exp(all_user_pairs / self.tau).sum(axis=1)
         cl_user_loss = torch.sum(-torch.log(pos_user_pairs/(all_user_pairs - pos_user_pairs)))
 
-        item_index = np.array(items).flatten()
-        items_noise_1 = items_noise_1[item_index]
-        items_noise_2 = items_noise_2[item_index]
-        items_noise_1 /= torch.norm(items_noise_1, p=2, dim=1, keepdim=True)
-        items_noise_2 /= torch.norm(items_noise_2, p=2, dim=1, keepdim=True)
+        items_noise_1 = items_noise_1[items]
+        items_noise_2 = items_noise_2[items]
+        items_noise_1_norm = items_noise_1 / torch.norm(items_noise_1, p=2, dim=1, keepdim=True)
+        items_noise_2_norm = items_noise_2 / torch.norm(items_noise_2, p=2, dim=1, keepdim=True)
 
-        pos_item_pairs = torch.sum(items_noise_1 * items_noise_2, axis=1)
+        pos_item_pairs = torch.sum(items_noise_1_norm * items_noise_2_norm, axis=1)
         pos_item_pairs = torch.exp(pos_item_pairs / self.tau)
-        all_item_pairs = torch.matmul(items_noise_1, items_noise_2.T)
+        all_item_pairs = torch.matmul(items_noise_1_norm, items_noise_2_norm.T)
         all_item_pairs = torch.exp(all_item_pairs / self.tau).sum(axis=1)
         cl_item_loss = torch.sum(-torch.log(pos_item_pairs/(all_item_pairs - pos_item_pairs)))
 
-        return cl_user_loss + cl_item_loss
+        return self.lam* (cl_user_loss + cl_item_loss)
 
--- train_simGCL.py
+++ train_simGCL.py
@@ -1,32 +1,35 @@
 import torch
 from config import Config
-from lightGCN import LightGCN, Dataset
+from lightGCN import Dataset
+from simGCL import SimGCL
 import numpy as np
 cfg = Config()
 yelp2018 = Dataset("./data/yelp2018/")
-agent = LightGCN(yelp2018, cfg)
+agent = SimGCL(yelp2018, cfg)
 optimizer = torch.optim.Adam(agent.parameters(), lr=cfg.lr, weight_decay=cfg.weight_decay)
 batch_size = cfg.user_sample * (cfg.neg_ratio + 1) * cfg.pos_number
 num_batch = yelp2018.userItemMatrix.count_nonzero() // batch_size + 1
 for ep in range(cfg.epoch):
     for batch in range(num_batch):
         users_index = np.random.randint(low=0, high=yelp2018.num_user, size=cfg.user_sample)
+        all_items = []
         pos_items = []
         neg_items = []
         for index, user_index in enumerate(users_index):
             all_connected_items = yelp2018.userItemMatrix[user_index].nonzero()[1]
             pos_item_user = np.random.choice(all_connected_items, size=cfg.pos_number)
             pos_items.append(pos_item_user)
+            all_items.append(pos_item_user.item())
             neg_item_user = []
             while len(neg_item_user) < cfg.pos_number * cfg.neg_ratio:
                 item = np.random.choice(yelp2018.num_item)
                 if item not in all_connected_items:
                     neg_item_user.append(item)
+                    all_items.append(item)
             neg_items.append(neg_item_user)
-
-        loss = agent.bpr_loss(users_index, pos_items, neg_items)
+        loss = agent.bpr_loss(users_index, pos_items, neg_items) + agent.batch_cl_loss(users_index, np.array(all_items))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         print(f"Ep {ep} batch {batch}: Loss {loss.item():.4f}")
-    torch.save(agent.state_dict(), "models/lgcn.pt")
+    torch.save(agent.state_dict(), "models/jgcf.pt")
